5bbaa753203f03cac7e9486f6c0cc8322904a5a1
xuehuiniaoyu <469490037@qq.com>
2019-04-17 15:43:28 +0800

    update

--- baymax-library/src/main/java/com/disney4a/baymax/core/app/application/BaymaxApplication.java
+++ baymax-library/src/main/java/com/disney4a/baymax/core/app/application/BaymaxApplication.java
@@ -45,21 +45,17 @@ public class BaymaxApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        init();
-    }
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-    }
-
-    void init() {
-        Baymax.initialize(this);
+		Baymax.initialize(base);
         // 先检查是否有注解
         Tag_Application application = this.getClass().getAnnotation(Tag_Application.class);
         if(application != null) {
             String[] annotationsPackages = application.annotationsPackages();
             Baymax.single().setAnnotationsPackage(annotationsPackages).play();
         }
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
     }
 }
